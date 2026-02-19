6cf63ca6e8f845ac67201739a83a466871726c39
Rogério Araújo <rogerio.araujo@gmail.com>
2026-02-19 08:48:18 -0300

    refactor: allow customize async runtime from config file

--- vetis/src/main.rs
+++ vetis/src/main.rs
@@ -7,16 +7,16 @@ use serde::Deserialize;
 use smol_macros::main;
 use std::{error::Error, fs::read_to_string, path::Path};
 use vetis::{
-    config::server::{
-        virtual_host::{path::static_files::StaticPathConfig, VirtualHostConfig},
-        ListenerConfig, ServerConfig,
-    },
+    config::server::{virtual_host::VirtualHostConfig, ServerConfig},
     server::virtual_host::VirtualHost,
     Vetis,
 };
 
 #[derive(Deserialize)]
 pub struct VetisServerConfig {
+    log_level: String,
+    workers: usize,
+    max_blocking_threads: usize,
     server: ServerConfig,
     virtual_hosts: Vec<VirtualHostConfig>,
 }
@@ -43,33 +43,57 @@ struct Args {
     config: Option<String>,
 }
 
-async fn run() -> Result<(), Box<dyn Error>> {
-    env_logger::Builder::from_env(env_logger::Env::default().filter_or("RUST_LOG", "info"))
-        .format_module_path(false)
-        .init();
+async fn run(
+    server_config: ServerConfig,
+    virtual_hosts_config: Vec<VirtualHostConfig>,
+) -> Result<(), Box<dyn Error>> {
+    let mut server = Vetis::new(server_config);
 
+    for virtual_host in virtual_hosts_config {
+        let virtual_host = VirtualHost::new(virtual_host);
+
+        server
+            .add_virtual_host(virtual_host)
+            .await;
+    }
+
+    if let Err(e) = server.run().await {
+        error!("Failed to start server: {}", e);
+    }
+
+    Ok(())
+}
+
+fn init_runtime() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
     if let Some(config) = args.config {
         if Path::exists(Path::new(&config)) {
             let file = read_to_string(&config);
+            if let Err(e) = file {
+                return Err(e.into());
+            }
+
             if let Ok(file) = file {
                 let config = serde_yaml_ng::from_str::<VetisServerConfig>(&file);
+                if let Err(e) = config {
+                    return Err(e.into());
+                }
+
                 if let Ok(config) = config {
-                    let mut server = Vetis::new(config.server);
+                    env_logger::Builder::from_env(
+                        env_logger::Env::default().filter_or("RUST_LOG", config.log_level),
+                    )
+                    .format_module_path(false)
+                    .init();
 
-                    for virtual_host in config.virtual_hosts {
-                        let virtual_host = VirtualHost::new(virtual_host);
-
-                        server
-                            .add_virtual_host(virtual_host)
-                            .await;
-                    }
-
-                    if let Err(e) = server.run().await {
-                        error!("Failed to start server: {}", e);
-                    }
+                    let rt = tokio::runtime::Builder::new_multi_thread()
+                        .enable_all()
+                        .worker_threads(config.workers)
+                        .max_blocking_threads(config.max_blocking_threads)
+                        .build()?;
+                    rt.block_on(async { run(config.server, config.virtual_hosts).await })?;
                 } else {
-                    error!(
+                    eprintln!(
                         "Failed to parse config file: {}",
                         config
                             .err()
@@ -78,57 +102,23 @@ async fn run() -> Result<(), Box<dyn Error>> {
                 }
             }
         }
-    } else {
-        let listener = ListenerConfig::builder()
-            .port(8080)
-            .build()?;
-
-        let server_config = ServerConfig::builder()
-            .add_listener(listener)
-            .build()?;
-
-        let mut server = Vetis::new(server_config);
-
-        let static_path_config = StaticPathConfig::builder()
-            .uri("/static")
-            .extensions("\\.(jpg|png|gif|html|css|js)$")
-            .directory(".")
-            .index_files(vec!["index.html".to_string()])
-            .build()?;
-
-        let virtual_host_config = VirtualHostConfig::builder()
-            .hostname("localhost")
-            .port(8080)
-            .static_paths(vec![static_path_config])
-            .build()?;
-
-        let virtual_host = VirtualHost::new(virtual_host_config);
-
-        server
-            .add_virtual_host(virtual_host)
-            .await;
-
-        if let Err(e) = server.run().await {
-            error!("Failed to start server: {}", e);
-        }
     }
+
     Ok(())
 }
 
 #[cfg(feature = "tokio-rt")]
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    if let Err(e) = run().await {
-        error!("Failed to start server: {}", e);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    if let Err(e) = init_runtime() {
+        eprintln!("Failed to start server: {}", e);
     }
     Ok(())
 }
 
 #[cfg(feature = "smol-rt")]
-#[apply(main!)]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    if let Err(e) = run().await {
-        error!("Failed to start server: {}", e);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    if let Err(e) = init_runtime() {
+        eprintln!("Failed to start server: {}", e);
     }
     Ok(())
 }
